e33c93b7f618cbf0f381a71f4f2afdc4678c3ea2
guldron-edu <58664019+guldron-edu@users.noreply.github.com>
2020-07-17 22:54:18 +0300

    add fix

--- src/index.js
+++ src/index.js
@@ -1,35 +1,6 @@
-const refs = {
-  days: document.querySelector('span[data-value="days"]'),
-  hours: document.querySelector('span[data-value="hours"]'),
-  mins: document.querySelector('span[data-value="mins"]'),
-  secs: document.querySelector('span[data-value="secs"]'),
-};
+import CountdownTimer from './js/CountdownTimer';
 
-const targetDate = Date.parse('Jul 17, 2021');
-
-const step = () => {
-  const currentDate = Date.now();
-  let delta = targetDate - currentDate;
-  const counter = {
-    days: Math.floor(delta / (1000 * 60 * 60 * 24)),
-    hours: Math.floor((delta % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-    mins: Math.floor((delta % (1000 * 60 * 60)) / (1000 * 60)),
-    secs: Math.floor((delta % (1000 * 60)) / 1000),
-  };
-
-  refs.days.textContent = pad(counter.days);
-  refs.hours.textContent = pad(counter.hours);
-  refs.mins.textContent = pad(counter.mins);
-  refs.secs.textContent = pad(counter.secs);
-};
-
-const pad = value => {
-  return String(value).padStart(2, '0');
-};
-
-const startTimer = () => {
-  step();
-  setInterval(step, 1000);
-};
-
-startTimer();
+new CountdownTimer({
+  selector: '#timer-1',
+  targetDate: new Date('Jul 17, 2021'),
+});
